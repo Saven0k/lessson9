00799e9dd618104fbb0f7c6880d68badc24d25cd
Savenok <Romasav2017@gmail.com>
2022-12-26 14:03:11 +0300

    view

---
+++ dz/bot_telegram_phoobook/view.py
@@ -0,0 +1,14 @@
+import log
+
+def read_write():
+    with open('phonesbook.txt', 'r') as f:
+        data = str(f"{''.join(f.read())}\n")
+        log.wr2('Прочитана инфа из справочника')
+    return data
+
+def oppen():
+    with open('phonesbook.txt', 'r') as f:
+        data = f.read().splitlines()
+       # print(f'{data}\n')
+
+        return data
