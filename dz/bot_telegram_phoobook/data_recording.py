ad1b687e15e00ea7042deef0fc3d32d1919d57da
Savenok <Romasav2017@gmail.com>
2023-01-11 08:55:55 +0300

    2.0

--- dz/bot_telegram_phoobook/data_recording.py
+++ dz/bot_telegram_phoobook/data_recording.py
@@ -1,13 +1,16 @@
 import log
 import time
 tm = time.asctime()
-def write(scn , nm , ph , inf, phonebook):
+def write(scn, nm, ph, inf):
+    phonebook = {}
     log.wr2('Создан справочник')
-    phonebook[f'{scn}'] = scn, nm, ph , inf, tm
+    phonebook[f'{scn}'] = nm, ph, inf, tm
     log.wr2('Добавленны данные в  справочник')
     save_phones(phonebook)
-    return phonebook
 
+
+def ww(data):
+    save_phones(data)
 def save_phones(data):
-    with  open('phonesbook.txt' , 'a') as f:
+    with open('phonesbook.txt', 'a') as f:
         f.write(f'{data}\n')
